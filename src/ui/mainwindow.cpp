d9d3264a8fd789da13e7aa9b33def45720de30af
Krishna Das <krishna.das@outlook.in>
2026-07-23 19:13:30 +0530

    Add patch file loading support
    Add support for loading and parsing git patch files.
    - Add File menu with Open action
    - Load patch files using QFileDialog
    - Parse selected patch using PatchParser
    - Display parsed patch summary
    
    Signed-off-by: Krishna Das <krishna.das@outlook.in>

--- src/ui/mainwindow.cpp
+++ src/ui/mainwindow.cpp
@@ -1,20 +1,78 @@
 #include "mainwindow.h"
 
-#include <QLabel>
+#include "../core/patch/patchparser.h"
+
+#include <QAction>
+#include <QFile>
+#include <QFileDialog>
+#include <QMenu>
+#include <QMenuBar>
+#include <QMessageBox>
+#include <QTextStream>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
     setWindowTitle("PatchLens");
+    resize(1200, 800);
 
-    auto *label = new QLabel(
-        "PatchLens - Git Patch Viewer",
-        this
-    );
+    createMenus();
+}
 
-    label->setAlignment(Qt::AlignCenter);
+void MainWindow::createMenus()
+{
+    QMenu *fileMenu = menuBar()->addMenu("&File");
 
-    setCentralWidget(label);
+    QAction *openAction = fileMenu->addAction("&Open Patch...");
+    connect(openAction, &QAction::triggered,
+            this, &MainWindow::openPatch);
 
-    resize(800, 600);
+    fileMenu->addSeparator();
+
+    QAction *exitAction = fileMenu->addAction("E&xit");
+    connect(exitAction, &QAction::triggered,
+            this, &QWidget::close);
+}
+
+void MainWindow::openPatch()
+{
+    QString fileName =
+        QFileDialog::getOpenFileName(
+            this,
+            "Open Patch",
+            QString(),
+            "Patch Files (*.patch *.diff);;All Files (*)");
+
+    if (fileName.isEmpty())
+        return;
+
+    QFile file(fileName);
+
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        QMessageBox::critical(
+            this,
+            "Error",
+            "Failed to open patch file.");
+
+        return;
+    }
+
+    QTextStream stream(&file);
+    QString patchText = stream.readAll();
+
+    PatchParser parser;
+    currentPatch = parser.parse(patchText);
+
+    QString summary;
+
+    summary += "Subject : " + currentPatch.subject + "\n";
+    summary += "Author  : " + currentPatch.author + "\n";
+    summary += "Date    : " + currentPatch.date + "\n";
+    summary += "Files   : " + QString::number(currentPatch.files.size());
+
+    QMessageBox::information(
+        this,
+        "Patch Loaded",
+        summary);
 }
--- src/ui/mainwindow.h
+++ src/ui/mainwindow.h
@@ -2,10 +2,20 @@
 
 #include <QMainWindow>
 
+#include "../core/patch/patchmodel.h"
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
     explicit MainWindow(QWidget *parent = nullptr);
+
+private slots:
+    void openPatch();
+
+private:
+    void createMenus();
+
+    PatchFile currentPatch;
 };
